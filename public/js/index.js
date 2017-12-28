7d94604b71fe49d3233143741dfab12e5e319dac
yuanye <yuanye@funhr.com>
2017-12-28 10:38:00 +0800

    fix swiper img

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 $(function () {
+    // swiper
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
@@ -8,53 +9,34 @@ $(function () {
         nextButton: '.arrow_btn_right',
         prevButton: '.arrow_btn_left'
     })
+
     // 把电影信息添加到div中
     var str1 = $('.movie_hidInfo').text();
     $('.movie_info').html(str1);
-    $('.index').on('click', function() {
-        setTimeout(function() {
-            window.location.href = '/index';
-        }, 350);
-    });
-    $('.action').on('click', function() {
-        setTimeout(function() {
-            window.location.href = '/action';
-        }, 350);
-    });
-    $('.comedy').on('click', function() {
-        setTimeout(function() {
-            window.location.href = '/comedy';
-        }, 350);
-    });
-    $('.affectional').on('click', function() {
-        setTimeout(function() {
-            window.location.href = '/affectional';
-        }, 350);
-    });
-    $('.science_fiction').on('click', function() {
-        setTimeout(function() {
-            window.location.href = '/science_fiction';
-        }, 350);
-    });
-    $('.dracula').on('click', function() {
-        setTimeout(function() {
-            window.location.href = '/dracula';
-        }, 350);
-    });
-    $('.comments').on('click', function() {
-        setTimeout(function() {
-            window.location.href = '/comments';
+
+    // header延迟跳转
+    $('.tab').find('a').on('click', function () {
+        var _this = this
+        setTimeout(function () {
+            window.location.href = _this.href;
         }, 350);
     });
 
     // 留言求片
-    $('.comment').on('click', function(event) {
+    $('.comment').on('click', function (event) {
         event.preventDefault();
         if ($('.comment_name').val() && $('.comment_content').val()) {
-            console.log('可以发送');
             $('.comment_form').submit();
-        }else{
+        } else {
             alert('请填写完整的信息')
         }
     });
+
+    // 处理图片错误展示
+    $('img').one('load', function () {
+        if (!/dexiazai/g.test(this.src)) {
+            $(this).remove()
+        }
+    });
+
 });
